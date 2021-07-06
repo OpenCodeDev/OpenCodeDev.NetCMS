34c64affc66064cab8b71f2747d52793ea628358
Max Samson <m.samson@flawlessloop.com>
2021-07-06 11:44:35 -0600

    Start Unit Testing Coverage

--- .netcms_config/templates/ApiControllerLogic.cs
+++ .netcms_config/templates/ApiControllerLogic.cs
@@ -39,41 +39,44 @@ namespace _NAMESPACE_BASE_SERVER_.Api._API_NAME_.Controllers
 
         public virtual  async Task<List<_API_NAME_PublicModel>> Fetch(_API_NAME_FetchRequest request, CallContext context = default)
         {
-            // Permissions and Access Control are being handled in the Wrapper of this class.
-            var provider = context.ServerCallContext.GetHttpContext().RequestServices;
-            var db = provider.GetRequiredService<ApiDatabase>();
-            var myService = provider.GetRequiredService<_API_NAME_MyService>();
+            // // Permissions and Access Control are being handled in the Wrapper of this class.
+            // var provider = context.ServerCallContext.GetHttpContext().RequestServices;
+            // var db = provider.GetRequiredService<ApiDatabase>();
+            // var myService = provider.GetRequiredService<_API_NAME_MyService>();
 
-            // Building Predicate Conditions
-            // Note: Reflection is being used and can be heavy when very large list of condition is given.
-            // you may want to restrict the number of condition, duplicate is allowed so the intent to slow the system can be used.
-            // By Default we allow 20 conditions longer than that will throw an error to protect the default system.
-            var predicate = myService.ConditionsPredicateBuilder(request.Conditions);
+            // // Building Predicate Conditions
+            // // Note: Reflection is being used and can be heavy when very large list of condition is given.
+            // // you may want to restrict the number of condition, duplicate is allowed so the intent to slow the system can be used.
+            // // By Default we allow 20 conditions longer than that will throw an error to protect the default system.
+            // var predicate = myService.ConditionsPredicateBuilder(request.Conditions);
 
-            // Execute Predicate on Database.
-            var result = db._API_NAME_.Where(p => predicate(p)).Select(p=>(_API_NAME_PublicModel)p).ToList();
-            return result;
+            // // Execute Predicate on Database.
+            // var result = db._API_NAME_.Where(p => predicate(p)).Select(p=>(_API_NAME_PublicModel)p).ToList();
+            // return result;
+             throw new NotImplementedException();
         }
 
         public virtual async Task<_API_NAME_PublicModel> FetchOne(_API_NAME_FetchOneRequest request, CallContext context = default)
         {
-            var provider = context.ServerCallContext.GetHttpContext().RequestServices;
-            var db = provider.GetRequiredService<ApiDatabase>();
-            var result = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
-            return result;
+            // var provider = context.ServerCallContext.GetHttpContext().RequestServices;
+            // var db = provider.GetRequiredService<ApiDatabase>();
+            // var result = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
+            // return result;
+             throw new NotImplementedException();
         }
 
         public virtual async Task<_API_NAME_PublicModel> Update(_API_NAME_UpdateOneRequest request, CallContext context = default)
         {
-            var provider = context.ServerCallContext.GetHttpContext().RequestServices;
-            var db = provider.GetRequiredService<ApiDatabase>();
-            var myService = provider.GetRequiredService<_API_NAME_MyService>();
-            var updating = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
-            // Map Fields One by One (No JSON, No Serializer, Simply Hard Handed... but Generated so... :D)
-            updating = myService.FilterUpdate(updating, (_API_NAME_Model)request.Element);
-            // Process References Update, Remove Unwanted, Add Wanted, Keep the Keeper
-            updating = myService.FilterUpdateReferences(db, updating, request);
-            return updating;
+            // var provider = context.ServerCallContext.GetHttpContext().RequestServices;
+            // var db = provider.GetRequiredService<ApiDatabase>();
+            // var myService = provider.GetRequiredService<_API_NAME_MyService>();
+            // var updating = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
+            // // Map Fields One by One (No JSON, No Serializer, Simply Hard Handed... but Generated so... :D)
+            // updating = myService.FilterUpdate(updating, (_API_NAME_Model)request.Element);
+            // // Process References Update, Remove Unwanted, Add Wanted, Keep the Keeper
+            // updating = myService.FilterUpdateReferences(db, updating, request);
+            // return updating;
+             throw new NotImplementedException();
         }
 
         public virtual async Task<List<_API_NAME_PublicModel>> UpdateMany(_API_NAME_UpdateManyRequest request, CallContext context = default)
---
+++ OpenCodeDev.NetCMS.Server.Test/ServiceCoreTest.cs
@@ -0,0 +1,178 @@
+﻿using Grpc.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenCodeDev.NetCMS.Core.Server.Api;
+using OpenCodeDev.NetCMS.Core.Shared.Api.Messages;
+using OpenCodeDev.NetCMS.Core.Shared.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace OpenCodeDev.NetCMS.Server.Test
+{
+    [TestClass]
+    public class ServiceCoreTest : ApiServiceBase
+    {
+
+        [TestMethod]
+        public void Test_Greather_Than_True_1()
+        {
+            Assert.IsTrue(GreaterThan(true, false));
+        }
+
+        [TestMethod]
+        public void Test_Greather_Than_True_2()
+        {
+            Assert.IsFalse(GreaterThan(false, true));
+        }
+
+        [TestMethod]
+        public void Test_Greather_Than_Int_1()
+        {
+            Assert.IsTrue(GreaterThan(3, 2));
+        }
+
+        [TestMethod]
+        public void Test_Greather_Than_Int_2()
+        {
+            Assert.IsFalse(GreaterThan(3, 6));
+        }
+
+        [TestMethod]
+        public void Test_Greather_Than_Int_3()
+        {
+            Assert.IsFalse(GreaterThan(3, 3));
+        }
+
+        [TestMethod]
+        public void Test_Predicate_Builder(){
+            Guid predicedID = Guid.NewGuid();
+            List<TestModel> DbSet = new List<TestModel>() { 
+                new TestModel() { Id = predicedID, Duration = 1, Name = "Half-Crooks" },
+                new TestModel() { Id = Guid.NewGuid(), Duration = 1, Name = "NetCMS.OpenCodeDev.com" },
+                new TestModel() { Id = Guid.NewGuid(), Duration = 1, Name = "Half-Way-Crooked" },
+                new TestModel() { Id = Guid.NewGuid(), Duration = 2, Name = "Fork-Repos" },
+                new TestModel() { Id = Guid.NewGuid(), Duration = 1, Name = "MS-Test" },
+                new TestModel() { Id = Guid.NewGuid(), Duration = 1, Name = "Test-Name" }
+            };
+            TestFetchRequest conditions = new TestFetchRequest() { 
+                Conditions = new List<TestPredicateConditions>() { 
+                 new TestPredicateConditions(){ Type= FieldTypes.Int, Conditions = ConditionTypes.GreaterThan, Field = TestPredicateConditions.Fields.Duration, NextLogic = LogicTypes.And, Value = "1" }
+                },
+                Limit = 10
+            };
+
+            var predicate = ConditionsPredicateBuilder(conditions.Conditions);
+            try
+            {
+                List<TestModel> result = DbSet.Where(p => predicate(p)).ToList();
+                throw new Exception(result.Count.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+         
+        }
+
+
+        /// <summary>
+        /// Condition Search Predicate Builder for Context of RecipeController
+        /// </summary>
+        /// <param name="conditions">List of condition</param>
+        public virtual Predicate<TestPublicModel> ConditionsPredicateBuilder(List<TestPredicateConditions> conditions)
+        {
+            Type _PublicModel = typeof(TestPublicModel);
+            Expression<Func<TestPublicModel, bool>> predicate = null;
+            LogicTypes nextLogicToFollow = LogicTypes.And;
+            bool nextFollowsLogic = false;
+            foreach (var item in conditions)
+            {
+                PropertyInfo fieldInfo = _PublicModel.GetPropertyInfoByName(item.Field.ToString()) // Get Property by Name
+                .ThrowWhenNull<PropertyInfo>(StatusCode.Unknown, $"The condition field {item.Field} is not available in the condition of fetch model. (You most likely need to update your client or wait several hours for next update deployment.");
+                fieldInfo.ValidationPropTypeAllowed() // Ensure Underlying type is allowed to be considered a condition.
+                .ThrowWhenFalse(StatusCode.InvalidArgument, $"The condition field {item.Field} is not supported type for condition of fetch.");
+
+                if (!nextFollowsLogic)
+                {
+                    predicate = p => ConditionTypeDelegator(item.Conditions, p.GetType().GetPropertyInfoByName(item.Field.ToString()).GetValue(p), item.Value, p.GetType().GetPropertyInfoByName(item.Field.ToString()).GetType());
+                }
+                else if (nextLogicToFollow == LogicTypes.And)
+                {
+                    predicate = predicate.And(p => ConditionTypeDelegator(item.Conditions, p.GetType().GetPropertyInfoByName(item.Field.ToString()).GetValue(p), item.Value, p.GetType().GetPropertyInfoByName(item.Field.ToString()).GetType()));
+                }
+                else
+                {
+                    predicate = predicate.Or(p => ConditionTypeDelegator(item.Conditions, p.GetType().GetPropertyInfoByName(item.Field.ToString()).GetValue(p), item.Value, p.GetType().GetPropertyInfoByName(item.Field.ToString()).GetType()));
+
+                }
+
+                nextFollowsLogic = true; // Next Loop will use nextLogic as predicate behavior
+            }
+
+            Func<TestPublicModel, bool> predFunc = predicate.Compile();
+            return p => predFunc(p);
+        }
+
+
+        // public virtual IQueryable<RecipePublicModel> RecipeLoadReference(IQueryable<NAMESPACE_BASE_SHARED.Api.Recipe.Models.RecipePublicModel> model, RecipeReferences references){
+        //     return model.Include(p => p.Ingredients);
+        // }
+
+
+        public virtual TestPublicModel FilterUpdate(TestModel current, TestModel changed)
+        {
+            //_UPDATE_FILTER_BODY_
+            return current;
+        }
+    }
+    public class TestPublicModel
+    {
+        public System.String Name { get; set; }
+        public System.Int32 Duration { get; set; }
+        public System.Guid Id { get; set; }
+
+    }
+
+    public class TestModel : TestPublicModel
+    {
+
+    }
+    public class TestFetchRequest
+    {
+        public List<TestPredicateConditions> Conditions { get; set; }
+        public System.Int32 Limit { get; set; }
+    }
+
+
+
+    public class TestPredicateConditions : ConditionBase
+    {
+        public Fields Field { get; set; }
+        public enum Fields
+        {
+            Id,
+            Name,
+            Duration
+        }
+
+        public Type GetFieldType()
+        {
+            switch (Field)
+            {
+                case Fields.Id:
+                    return typeof(System.Guid);
+                case Fields.Name:
+                    return typeof(System.String);
+                case Fields.Duration:
+                    return typeof(System.Int32);
+            }
+            throw new RpcException(new Status(StatusCode.Unknown, "Server misconfiguration tries to pass un supported type of data, contact support."));
+        }
+
+
+    }
+}
