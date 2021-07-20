d9b4ede5feceab14356294e5e7fe6b6f33e3ce00
Max Samson <m.samson@flawlessloop.com>
2021-07-20 09:26:56 -0600

    Update Logic Controller

--- .netcms_config/templates/ApiControllerLogic.cs
+++ .netcms_config/templates/ApiControllerLogic.cs
@@ -28,7 +28,7 @@ namespace _NAMESPACE_BASE_SERVER_.Api._API_NAME_.Controllers
     /// This class provides you with fully functional common api CRUD logic.<br/>
     /// You can also inherit this class and override the logic to use your own custom logic. (Not Recommended)
     /// </summary>
-    public class _API_NAME_Controller : I_API_NAME_Controller
+    public class _API_NAME_Controller
     {
         public virtual async Task<_API_NAME_PublicModel> Create(_API_NAME_CreateRequest request, CallContext context = default)
         {         
@@ -37,7 +37,15 @@ namespace _NAMESPACE_BASE_SERVER_.Api._API_NAME_.Controllers
 
         public virtual async Task<_API_NAME_PublicModel> Delete(_API_NAME_DeleteRequest request, CallContext context = default)
         {
-            throw new NotImplementedException();
+            var provider = context.ServerCallContext.GetHttpContext().RequestServices;
+            var db = provider.GetRequiredService<ApiDatabase>();
+            var result = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
+            if (result != null) { db._API_NAME_.Remove(result); }
+            else{
+                throw new RpcException(new Status(StatusCode.NotFound, "Cannot delete because entry wasn't found."));
+            }            
+            await db.SaveChangesAsync();
+            return result;
         }
 
         public virtual  async Task<List<_API_NAME_PublicModel>> Fetch(_API_NAME_FetchRequest request, CallContext context = default)
@@ -55,30 +63,30 @@ namespace _NAMESPACE_BASE_SERVER_.Api._API_NAME_.Controllers
 
         public virtual async Task<_API_NAME_PublicModel> FetchOne(_API_NAME_FetchOneRequest request, CallContext context = default)
         {
-            // var provider = context.ServerCallContext.GetHttpContext().RequestServices;
-            // var db = provider.GetRequiredService<ApiDatabase>();
-            // var result = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
-            // return result;
-             throw new NotImplementedException();
+            var provider = context.ServerCallContext.GetHttpContext().RequestServices;
+            var db = provider.GetRequiredService<ApiDatabase>();
+            var result = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
+            if (result == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Cannot find {request.Id} because entry wasn't found."));
+            }
+            return result;
         }
 
         public virtual async Task<_API_NAME_PublicModel> Update(_API_NAME_UpdateOneRequest request, CallContext context = default)
         {
-            // var provider = context.ServerCallContext.GetHttpContext().RequestServices;
-            // var db = provider.GetRequiredService<ApiDatabase>();
-            // var myService = provider.GetRequiredService<_API_NAME_MyService>();
-            // var updating = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
-            // // Map Fields One by One (No JSON, No Serializer, Simply Hard Handed... but Generated so... :D)
-            // updating = myService.FilterUpdate(updating, (_API_NAME_Model)request.Element);
-            // // Process References Update, Remove Unwanted, Add Wanted, Keep the Keeper
+            var provider = context.ServerCallContext.GetHttpContext().RequestServices;
+            var db = provider.GetRequiredService<ApiDatabase>();
+            var myService = provider.GetRequiredService<_API_NAME_MyService>();
+            var updating = db._API_NAME_.Where(p => p.Id.Equals(request.Id)).FirstOrDefault();
+            if(updating == null){
+                throw new RpcException(new Status(StatusCode.NotFound, $"Cannot update {request.Id} because entry wasn't found."));
+            }
+            updating = myService.FilterUpdate(updating, (_API_NAME_Model)request.Element);
             // updating = myService.FilterUpdateReferences(db, updating, request);
-            // return updating;
-             throw new NotImplementedException();
-        }
-
-        public virtual async Task<List<_API_NAME_PublicModel>> UpdateMany(_API_NAME_UpdateManyRequest request, CallContext context = default)
-        {
-            throw new NotImplementedException();
+            db._API_NAME_.Add(updating);
+            await db.SaveChangesAsync();
+            return updating;
         }
     }
 }
